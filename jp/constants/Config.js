fe81e5aae5a1f5a85251be14bcf92492f835d427
01527111 <01527111@student.tuwien.ac.at>
2018-10-21 19:10:05 +0200

    updated courses

--- jp/constants/Config.js
+++ jp/constants/Config.js
@@ -19,7 +19,35 @@ const testTheorie2_2 = {
   kanjiTo: 854,
 }
 
-const testList = [testTheorie1, testTheorie2, testTheorie2_2]
+const testTheorie3 = {
+  key: 3,
+  name: 'Theorie 3',
+  kanjiFrom: 855,
+  kanjiTo: 1100,
+}
+
+const testTheorie4 = {
+  key: 2,
+  name: 'Theorie 2',
+  kanjiFrom: 1101,
+  kanjiTo: 1500,
+}
+
+const testTheorie5 = {
+  key: 2,
+  name: 'Theorie 2',
+  kanjiFrom: 1501,
+  kanjiTo: 2137,
+}
+
+const testAll = {
+  key: 2,
+  name: 'Alle Kanji',
+  kanjiFrom: 1,
+  kanjiTo: 2137,
+}
+
+const testList = [testTheorie1, testTheorie2, testTheorie2_2, testTheorie3, testTheorie4, testTheorie5, testAll];
 
 export default testList;
 
