1c6f96719406127c0dc3bac0e0a91cd0ddc633fc
Flo <me@flo-lan.com>
2019-09-19 17:40:42 +0200

    copy kanji to clipboard on press

--- jp/components/KanjiCard/index.js
+++ jp/components/KanjiCard/index.js
@@ -5,7 +5,9 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  Clipboard,
 } from 'react-native';
+import Toast from 'react-native-easy-toast';
 
 import styles from './styles';
 import { totalQuestions } from '../../constants/Config';
@@ -14,17 +16,19 @@ class KanjiCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.answerPressed = this.answerPressed.bind(this);
     this.handleShow = this.handleShow.bind(this);
+    this.kanjiPressed = this.kanjiPressed.bind(this);
     this.renderONReadings = this.renderONReadings.bind(this);
     this.renderKUNReadings = this.renderKUNReadings.bind(this);
     this.renderMeaning = this.renderMeaning.bind(this);
 
     this.state = {
       showAnswer: false,
-    }
+    };
   }
 
-  handleAnswer(points) {
+  answerPressed(points) {
     const { handleAnswer } = this.props;
     this.setState({
       showAnswer: false,
@@ -32,15 +36,20 @@ class KanjiCard extends React.Component {
     handleAnswer(points);
   }
 
-  handleShow() { 
+  handleShow() {
     this.setState({
       showAnswer: true,
     });
   }
 
+  async kanjiPressed(kanjiSymbol) {
+    await Clipboard.setString(kanjiSymbol);
+    this.refs.toast.show(`Copied ${kanjiSymbol} to clipboard`);
+  }
+
   renderONReadings(curKanji) {
-    return(
-      <View style={{flex:1}}>
+    return (
+      <View style={{ flex: 1 }}>
         <Text style={styles.headline}>
           ON
         </Text>
@@ -57,8 +66,8 @@ class KanjiCard extends React.Component {
   }
 
   renderKUNReadings(curKanji) {
-    return(
-      <View style={{flex:1}}>
+    return (
+      <View style={{ flex: 1 }}>
         <Text style={styles.headline}>
           KUN
         </Text>
@@ -85,89 +94,93 @@ class KanjiCard extends React.Component {
           <Text style={[styles.text, styles.comment]}>
             {`(${meaning})`}
           </Text>
-         </View>
+        </View>
       );
     }
 
     return null;
   }
-  
+
   render() {
     const { curQuestion, curKanji } = this.props;
     const { showAnswer } = this.state;
 
     return (
       <View style={styles.container}>
-       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <Text style={styles.questionCounter}>
             {`${curQuestion} / ${totalQuestions}`}
           </Text>
-          <Text selectable style={styles.kanji}>
+          <Text selectable style={styles.kanji} onPress={() => this.kanjiPressed(curKanji.symbol)}>
             {curKanji.symbol}
           </Text>
-          { showAnswer && 
-            <View>
-              <View style={styles.divider} />
-              <Text style={styles.headline}>
-                Radikal
-              </Text>
-              <Text selectable style={styles.text}>
-                {curKanji.radical}
-              </Text>
-              <Text style={styles.headline}>
-                Bedeutung
-              </Text>
-              <Text style={styles.text}>
-                {curKanji.bedeutung}
-              </Text>
-              <View style={styles.readingContainer}>
-                {this.renderKUNReadings(curKanji)}
-                {this.renderONReadings(curKanji)}
-              </View>
+          { showAnswer
+          && (
+          <View>
+            <View style={styles.divider} />
+            <Text style={styles.headline}>
+              Radikal
+            </Text>
+            <Text selectable style={styles.text}>
+              {curKanji.radical}
+            </Text>
+            <Text style={styles.headline}>
+              Bedeutung
+            </Text>
+            <Text style={styles.text}>
+              {curKanji.bedeutung}
+            </Text>
+            <View style={styles.readingContainer}>
+              {this.renderKUNReadings(curKanji)}
+              {this.renderONReadings(curKanji)}
             </View>
-          }
+          </View>
+          )}
         </ScrollView>
-        { !showAnswer && 
+        { !showAnswer
+          && (
           <TouchableOpacity
-            activeOpacity={.3} 
+            activeOpacity={0.3}
             style={styles.showButton}
             onPress={this.handleShow}
           >
             <Text style={styles.buttonText}>SHOW</Text>
           </TouchableOpacity>
-        }
-        { showAnswer && 
+          )}
+        { showAnswer
+          && (
           <View style={styles.pointsContainer}>
             <TouchableOpacity
-              activeOpacity={.3} 
+              activeOpacity={0.3}
               style={[styles.pointButton, styles.point0Button]}
-              onPress={() => this.handleAnswer(0)}
+              onPress={() => this.answerPressed(0)}
             >
               <Text style={styles.buttonText}>0P</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              activeOpacity={.3} 
+              activeOpacity={0.3}
               style={[styles.pointButton, styles.point1Button]}
-              onPress={() => this.handleAnswer(1)}
+              onPress={() => this.answerPressed(1)}
             >
               <Text style={styles.buttonText}>1P</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              activeOpacity={.3} 
+              activeOpacity={0.3}
               style={[styles.pointButton, styles.point2Button]}
-              onPress={() => this.handleAnswer(2)}
+              onPress={() => this.answerPressed(2)}
             >
-             <Text style={styles.buttonText}>2P</Text>
+              <Text style={styles.buttonText}>2P</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              activeOpacity={.3} 
+              activeOpacity={0.3}
               style={[styles.pointButton, styles.point3Button]}
-              onPress={() => this.handleAnswer(3)}
+              onPress={() => this.answerPressed(3)}
             >
               <Text style={styles.buttonText}>3P</Text>
             </TouchableOpacity>
-        </View>
-        }
+          </View>
+          )}
+        <Toast ref="toast" positionValue={200} />
       </View>
     );
   }
@@ -177,6 +190,6 @@ KanjiCard.propTypes = {
   curQuestion: PropTypes.number.isRequired,
   curKanji: PropTypes.object.isRequired,
   handleAnswer: PropTypes.func.isRequired,
-}
+};
 
 export default KanjiCard;
