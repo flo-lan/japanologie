cd9e1c481a292775a749f88080c3cfef43893cfa
flo <flo.langeder@gmail.com>
2020-10-14 01:27:43 +0200

    split kanji overview in tabs of 100

--- jp/components/KanjiOverview/index.js
+++ jp/components/KanjiOverview/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView, Text, Button } from 'react-native';
+import { Container, Header, Content, Tab, Tabs, ScrollableTab } from 'native-base';
 import { Col, Row, Grid } from "react-native-easy-grid";
 
 import KanjiBlock from '../KanjiBlock';
@@ -8,15 +9,19 @@ import kanjiList from '../../data/kanjiList';
 
 import styles from './styles';
 
+const KANJI_PER_TAB = 100;
+
 class KanjiOverview extends React.Component {
   constructor(props) {
     super(props);
 
     this.renderRow = this.renderRow.bind(this);
-
+    this.renderTab = this.renderTab.bind(this);
     this.state = {
       relevantKanji: [],
       cols: 5,
+      activeTab: 0,
+      initialTab: 0,
     };
   }
 
@@ -39,9 +44,25 @@ class KanjiOverview extends React.Component {
     );
   }
 
+  renderTab(kanjiTab, i) {
+    const from = this.props.kanjiFrom + (i * KANJI_PER_TAB);
+    const to = Math.min(this.props.kanjiTo, this.props.kanjiFrom + (i * KANJI_PER_TAB + KANJI_PER_TAB - 1));
+    return (
+      <Tab heading={`${from}-${to}`}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <Grid>
+          {kanjiTab.map(kanjiRow => 
+            this.renderRow(kanjiRow)
+          )}
+          </Grid>
+        </ScrollView>
+      </Tab>
+    )
+  }
+
   render() {
     const { relevantKanji, cols } = this.state;
-    kanjiRows = [];
+    let kanjiRows = [];
     relevantKanji.forEach((kanji, i) => {
       let row = Math.floor(i / cols);
       if (kanjiRows.length <= row) {
@@ -49,14 +70,35 @@ class KanjiOverview extends React.Component {
       }
       kanjiRows[row].push(kanji);
     });
+
+    let i = 0;
+    let kanjiTabs = [];
+    kanjiRows.forEach(kanjiRow => {
+      i += kanjiRow.length;
+      let tab = Math.floor((i - 1) / KANJI_PER_TAB);
+      if (kanjiTabs.length <= tab) {
+        kanjiTabs[tab] = [];
+        // TODO: in uneven breaks split row between tabs
+      }
+
+      kanjiTabs[tab].push(kanjiRow);
+    });
+
     return (
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <Grid>
-          {kanjiRows.map((kanjiRow, i) => 
-            this.renderRow(kanjiRow)
+      // <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      //   <Grid>
+      //     {kanjiRows.map((kanjiRow, i) => 
+      //       this.renderRow(kanjiRow)
+      //     )}
+      //   </Grid>
+      // </ScrollView>
+      <Container>
+        <Tabs renderTabBar={()=> <ScrollableTab />}>
+          {kanjiTabs.map((kanjiTab, i) => 
+            this.renderTab(kanjiTab, i)
           )}
-        </Grid>
-      </ScrollView>
+        </Tabs>
+      </Container>
     );
   }
 }
