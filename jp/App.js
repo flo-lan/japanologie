37161e7cd35710966e08395f76a70b6b24c94794
flo <flo.langeder@gmail.com>
2020-10-15 16:22:19 +0200

    bumped expo to v33.0.0 to fix weird tab scrolling behaviour

--- jp/App.js
+++ jp/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Font, Icon } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
@@ -31,7 +33,7 @@ export default class App extends React.Component {
     return Promise.all([
       Font.loadAsync({
         // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
+        ...Ionicons.font,
       }),
     ]);
   };
